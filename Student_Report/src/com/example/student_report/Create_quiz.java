5b7bb23b8eda71ef2b199f13ed2cd9194856bddb
Aparna Guntaka <aparna.guntaka@gmail.com>
2014-09-09 03:23:58 +0530

    second commit

--- Student_Report/src/com/example/student_report/Create_quiz.java
+++ Student_Report/src/com/example/student_report/Create_quiz.java
@@ -27,25 +27,7 @@ public class Create_quiz extends ActionBarActivity {
     	
     }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.create_quiz, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 
 
 }
